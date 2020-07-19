7c8e3aee3000cbdbb45e30f2f12acde37fcee9bd
Sarthak Mishra <sarmsarthak@gmail.com>
2020-07-19 23:53:07 +0530

    Added Captured Time v/s Packet no

--- Packet_Capture/Len_No_script.py
+++ Packet_Capture/Len_No_script.py
@@ -13,8 +13,9 @@ logger = logging.getLogger(__name__)
 def plot(filename):
     pkt_arr = []
     cap = pyshark.FileCapture(filename, only_summaries=True)
-    
+
     pkt_arr.append((1, 66))
+
     print(cap)
     for packet in cap:
         # Create a plot point where (x=protocol, y=bytes)
@@ -23,12 +24,12 @@ def plot(filename):
     print(pkt_arr)
 
     # Create pygal instance
-    pkt_size_chart = XY(width=600, height=400, style=LightGreenStyle, explicit_size=True)
+    pkt_size_chart = XY(width=600, height=500, style=LightGreenStyle, explicit_size=True)
     pkt_size_chart.title = 'Packets Analysis'
-    # Add points to chart and render chart html
+
     pkt_size_chart.add('Size v/s No', pkt_arr)
-    #pkt_size_chart.add('Packet Length', pkt_sizes)       to add more graphs on same chart
     chart = pkt_size_chart.render()
+
     #print(chart)
     html = """{}""".format(chart)
     return html 
@@ -42,7 +43,7 @@ def main():
 
     filename = sys.argv[1]
     op2 = plot(filename)
-    htmlFile = open('output.html', 'w')
+    htmlFile = open('Len_No_output.html', 'w')
     htmlFile.write('<pre>')
     htmlFile.write(op2)
     htmlFile.write('</pre>')
---
+++ Packet_Capture/Time_No_script.py
@@ -0,0 +1,54 @@
+import sys
+import logging
+import subprocess
+import pyshark
+from datetime import datetime
+from flask import Flask, request, redirect, url_for
+from pygal import XY
+from pygal.style import LightGreenStyle
+
+logging.getLogger().setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def plot(filename):
+    pkt_arr = []
+    cap = pyshark.FileCapture(filename, only_summaries=True)
+
+    pkt_arr.append((1,0.0))
+
+    print(cap)
+    for packet in cap:
+        # Create a plot point where (x=protocol, y=bytes)
+        pkt_arr.append((int(packet.no), float(packet.time)))
+
+    print(pkt_arr)
+
+    # Create pygal instance
+    pkt_size_chart = XY(width=600, height=400, style=LightGreenStyle, explicit_size=True)
+    pkt_size_chart.title = 'Packets Analysis'
+
+    pkt_size_chart.add('Time v/s No', pkt_arr)
+    chart = pkt_size_chart.render()
+    
+
+    #print(chart)
+    html = """{}""".format(chart)
+    return html 
+
+def main():
+	
+    if len(sys.argv) != 2:
+    	logger.error('Insufficient arguments')
+    	print("Usage: <script_name>.py <pcap_file>")
+    	sys.exit(1)
+
+    filename = sys.argv[1]
+    op2 = plot(filename)
+    htmlFile = open('Time_No_output.html', 'w')
+    htmlFile.write('<pre>')
+    htmlFile.write(op2)
+    htmlFile.write('</pre>')
+
+
+if __name__=="__main__":
+    main()
